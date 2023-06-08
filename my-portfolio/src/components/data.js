65b223f0065dafaca18e133adea78c07b9388523
Liridona <91525128+barlaek@users.noreply.github.com>
2023-06-08 22:05:05 +0200

    cleanup

--- my-portfolio/src/components/data.js
+++ my-portfolio/src/components/data.js
@@ -7,11 +7,11 @@ export const data = [
   {
     title: 'The Cart & The Shop',
     description: 'E-commerce single page application',
-    image: '/images/e-commerce-screenshot(2).jpg',
+    image: './images/e-commerce-screenshot(2).jpg',
   },
   {
     title: 'Auctioneer',
     description: 'Auction website',
-    image: '/images/auctioneer-screenshot(2).jpg',
+    image: './images/auctioneer-screenshot(2).jpg',
   },
 ];
