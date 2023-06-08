9d7408b13a8020f415da6a44f8d41ddfadefc3a5
Liridona <91525128+barlaek@users.noreply.github.com>
2023-06-08 22:04:21 +0200

    debugging images not showing from data array

--- my-portfolio/src/components/Cards.js
+++ my-portfolio/src/components/Cards.js
@@ -1,11 +1,15 @@
 function Cards(prop) {
   const content = prop.data.map((card) => {
-    <div key={card.title}>
-      <img src={card.image} alt="image of website" />
-      <h2>{card.title}</h2>
-      <p>{card.description}</p>
-    </div>;
+    return (
+      <div key={card.title}>
+        <img src={card.image} alt="image of website" />
+        <h2>{card.title}</h2>
+        <p>{card.description}</p>
+      </div>
+    );
   });
+
+  console.log(content);
 
   return <div>{content}</div>;
 }
--- my-portfolio/src/components/data.js
+++ my-portfolio/src/components/data.js
@@ -2,16 +2,16 @@ export const data = [
   {
     title: 'Holidaze',
     description: 'Booking application',
-    image: './images/holidaze-screenshot(2).jpg',
+    image: '/my-portfolio/src/components/images/holidaze-screenshot(2).jpg',
   },
   {
     title: 'The Cart & The Shop',
     description: 'E-commerce single page application',
-    image: './images/e-commerce-screenshot(2).jpg',
+    image: '/images/e-commerce-screenshot(2).jpg',
   },
   {
     title: 'Auctioneer',
     description: 'Auction website',
-    image: './images/auctioneer-screenshot(2).jpg',
+    image: '/images/auctioneer-screenshot(2).jpg',
   },
 ];
