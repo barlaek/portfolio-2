5882e63c5b9749f390c37b03c0d6716e2bbe5c93
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-06-09 11:56:42 +0200

    card styling

--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { data } from './data';
 import Cards from './Cards';
+import styles from './styles/Portfolio.module.css';
 
 function Portfolio() {
   console.log(data);
-  return <Cards data={data} />;
+  return (
+    <div className={styles.content}>
+      <Cards data={data} />;
+    </div>
+  );
 }
 
 export default Portfolio;
