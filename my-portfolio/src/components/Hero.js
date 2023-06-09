e57cb7234fbb22bf691c35b67d48fd99aa0337c5
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-06-09 10:19:25 +0200

    styles

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './styles/Hero.module.css';
 
 function Hero() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Welcome, my name is Bjørn Arne</h1>
       <h2>And I am a Frontend Developer</h2>
     </div>
