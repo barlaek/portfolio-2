b1624f12359dc506fde85fd786932b7eb1efe592
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-06-09 11:18:10 +0200

    styles to hero container

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -4,7 +4,7 @@ import styles from './styles/Hero.module.css';
 function Hero() {
   return (
     <div className={styles.container}>
-      <h1>Welcome, my name is Bjørn Arne</h1>
+      <h1>Hi, my name is Bjørn Arne</h1>
       <h2>And I am a Frontend Developer</h2>
     </div>
   );
