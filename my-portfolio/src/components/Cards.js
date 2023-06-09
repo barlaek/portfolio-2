2abb3deb16f09822846101396ab25194bd120e97
Bjørn Arne Læknes <91525128+barlaek@users.noreply.github.com>
2023-06-09 11:41:27 +0200

    card styles

--- my-portfolio/src/components/Cards.js
+++ my-portfolio/src/components/Cards.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import styles from './styles/Cards.module.css';
 
 function Cards(prop) {
   const content = prop.data.map((card) => {
     return (
-      <div key={card.title}>
-        <img src={card.image} alt="image of website" />
-        <h2>{card.title}</h2>
-        <p>{card.description}</p>
+      <div key={card.title} className={styles.container}>
+        <img src={card.image} alt="image of website" className={styles.image} />
+        <div className={styles.textBox}>
+          <h2>{card.title}</h2>
+          <p>{card.description}</p>
+        </div>
       </div>
     );
   });
