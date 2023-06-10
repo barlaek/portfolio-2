7cf3a7b3a10c9f981c19395268fcd0955a3dd3ad
Liridona <91525128+barlaek@users.noreply.github.com>
2023-06-10 13:31:40 +0200

    parallax effect

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -3,9 +3,13 @@ import styles from './styles/Hero.module.css';
 
 function Hero() {
   return (
-    <div className={styles.container}>
-      <h1>Hi, my name is Bjørn Arne</h1>
-      <h2>And I am a Frontend Developer</h2>
+    <div className={styles.background}>
+      <div className={styles.mask}>
+        <div className={styles.container}>
+          <h1>Hi, my name is Bjørn Arne</h1>
+          <h2>And I am a Frontend Developer</h2>
+        </div>
+      </div>
     </div>
   );
 }
